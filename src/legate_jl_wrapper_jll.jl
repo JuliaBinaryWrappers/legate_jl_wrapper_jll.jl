88ed6d7d15ed6c77ca1cea31aaed80f7d47ae31d
jlbuild <juliabuildbot@gmail.com>
2026-01-09 14:46:20 +0000

    legate_jl_wrapper_jll build 25.10.1+0

--- src/legate_jl_wrapper_jll.jl
+++ src/legate_jl_wrapper_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("legate_jl_wrapper")
-JLLWrappers.@generate_main_file("legate_jl_wrapper", UUID("6e093c14-372f-5516-b4d7-a6ce14f1038b"))
+JLLWrappers.@generate_main_file("legate_jl_wrapper", Base.UUID("6e093c14-372f-5516-b4d7-a6ce14f1038b"))
 end  # module legate_jl_wrapper_jll
